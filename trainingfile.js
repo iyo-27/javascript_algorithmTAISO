c17d96b2174285075df18388663442b7fb2c084b
iyo-27 <iyo.nina27@gmail.com>
2023-11-21 14:25:31 +0000

    エラトステネスのふるい実装

--- trainingfile.js
+++ trainingfile.js
@@ -1,1 +1,15 @@
-consol.log("ハローワールド");
+
+let res = []
+a = []
+N = 20
+for (i=2; i<N; i++) a[i] = true
+for (i=2; i<N; i++) {
+ if(a[i]){
+    for (j=i*2; j<N; j+=i){
+        a[j] = false
+    }
+    res.push(i)
+ }
+}
+
+console.log(res);
