13cb5ee7895b5671c2b5d1b10ca68ad666bb84a1
iyo-27 <iyo.nina27@gmail.com>
2023-11-22 04:44:16 +0000

    0からn-1まで数え上げるプログラム実装

--- trainingfile.js
+++ trainingfile.js
@@ -1,21 +1,9 @@
-function vdiff(p1, p2){
-    var [x1, y1] = p1
-    var [x2, y2] = p2
-    return [x2-x1, y2-y1]
+function work(n){
+  var s = ""
+  for (var i=0; i<n; i++){
+    s += i + " "
+  }
+  return s
 }
 
-function prod(p1, p2){
-    var [x1, y1] = p1
-    var [x2, y2] = p2
-
-    return x1 * y2 - y1 * x2 
-}
-
-var menseki = (p1, p2, p3) =>
-  Math.abs(prod(vdiff(p3, p1), vdiff(p2, p1))) / 2
-
-var p1 = [3, 3]
-var p2 = [2, 1]
-var p3 = [1, 7]
-
-console.log(menseki(p1, p2, p3))
+console.log(work(10))
